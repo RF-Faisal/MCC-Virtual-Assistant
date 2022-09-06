0735d134b2f60fabfebc378224de2d4559d093d3
Ellora Yasi <ellora.yasi@gmail.com>
2022-09-06 20:18:26 +0600

    ..

--- default/db_conn.php
+++ default/db_conn.php
@@ -1,15 +1,7 @@
 <?php
-    $conn = oci_connect('mccSara', '123', '//localhost/XE');
+    $conn = oci_connect('m', 'm', '//localhost/XE');
     // Check connection
     if (!$conn) {
         echo 'Failed to connect to oracle' . "<br>";
-    }
-    else{
-        // echo 'Connected successfully';
-        // ?>
-        // <script>
-        //     alert("Connection successful");
-        // </script>
-        // <?php
     }
 ?>
